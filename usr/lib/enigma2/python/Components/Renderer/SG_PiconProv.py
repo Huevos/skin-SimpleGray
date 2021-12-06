45676895bad326820a4b1a9ce5fc42f1a8659663
Huevos <rowland@moralet.com>
2021-12-06 11:38:41 +0100

    [SG_PiconProv.py] fix import

--- usr/lib/enigma2/python/Components/Renderer/SG_PiconProv.py
+++ usr/lib/enigma2/python/Components/Renderer/SG_PiconProv.py
@@ -3,7 +3,7 @@ import os
 from enigma import ePixmap
 
 from Components.Harddisk import harddiskmanager
-from Components.Renderer import Renderer
+from Components.Renderer.Renderer import Renderer
 from Tools.Directories import pathExists
 
 
